99322179301b95e6dc5da341a585aeaead5d5b27
Maxwell <maxwellevin@users.noreply.github.com>
2017-09-19 12:53:40 -0700

    Add files via upload

--- Madelung Constant.py
+++ Madelung Constant.py
@@ -1,71 +1,44 @@
 # -*- coding: utf-8 -*-
 """
 Exercise 2.9: The Madelung constant
-TODO: Check if answer is meant to diverge as L->large
 
 Created on Sun Sep 17 11:37:17 2017
 
 @author: Maxwell
 """
 
-e = 1.60217662 * 10**(-19)
-perm_vac = 8.85 * 10**(-12)
-pi = 3.14159265359
-a = 1
-
-def atom_charge(i, j, k):
-    """ Returns e if there is a sodium atom, -e if chlorine """
-    if i + j + k % 2 == 0: return e
-    else: return -e
+# NOTE: This program is slow, but does not use much memory. 
+# May be able to increase speed by predetermining -1 ^ i+j+k,
+# manually accounting for i=j=k=0, and analyzing symmetries
 
 
 
-def atom_potential(i, j, k):
-    """ Returns the potential of an atom at i, j, k"""
-    numerator = atom_charge(i, j, k)
-    denominator = 4 * pi * perm_vac * a * (i**2 + j**2 + k**2)**(0.5)
-    return numerator / denominator
+# get L input
+L = int( input("Enter a value for L: ") )
+
+# initialize madelung
+M = 0
 
 
-
-def total_potential(L=10):
-    """ Returns the sum of all the potentials of atoms in the cubic lattice of length L"""
-    
-    sum_potential = 0
-    
-    # Summation
-    for i in range(-L, L):
-        
-        for j in range(-L, L):
-            
-            for k in range(-L, L):
-                
-                if i == j == k == 0:
-                    # do nothing
-                    continue
-                sum_potential += atom_potential(i, j, k)
-    
-    # Return our sum
-    return sum_potential
+# Loop through all integer lattice points in a cube of length 'L'
+# compute the contribution of each atom at each point and add to 'M'
+i = -L
+while i < L+1:
+    j = -L
+    while j < L+1:
+        k = -L
+        while k < L+1:
+            # Ignore the origin
+            if (i==0) and (j==0) and (k==0):
+                k += 1
+            else:
+                # If i+j+k even, atom is Na+, else Cl-
+                # Divide by the distance from current point to NaCl at origin
+                M += ((-1)**(i+j+k)) / ( i**2 + j**2 + k**2 )**0.5
+                k += 1
+        j += 1
+    i += 1
 
 
-def compute_Madelung(depth=10):
-    """ Approximates the Madelung Constant """
-    madelung = 4 * pi * perm_vac * a * total_potential(depth) / e
-    return madelung
-
-
-
-# ====== Test our program ======= #
-
-import datetime
-before = datetime.datetime.today()
-
-print (compute_Madelung(30))
-
-after = datetime.datetime.today()
-print("Time elapsed = ", after-before)
-
-
-
-    
+# Print the result
+print("The MadeLung Constant is approximately", M)
