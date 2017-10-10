bd3b7888c4257de8e915dd340440cd336f80937d
Maxwell <maxwellevin@users.noreply.github.com>
2017-10-10 15:12:24 -0700

    Add files via upload

---
+++ Exercise 5.14.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  8 19:25:17 2017
+
+@author: Maxwell, with help from Evan Davidson and Eli Barnes
+"""
+import numpy as np
+import pylab as pl
+
+# Declare our constants
+# a --> d are our bounds of integration / size of the plate
+a = -5
+b = 5
+c = -5
+d = 5
+G = 6.674e-11
+sigma = 100
+
+# Number of iterations
+N = 100
+
+# Function inside our double integral
+def func():
+    return lambda x,y,z: (x**2+y**2+z**2)**(-3/2)
+
+# Function to compute 2-dimensional integration with the Trapezoidal Rule
+def trapezoidal_2D(z, n=N, f = func()) :
+    h = (b - a) / n
+    k = (d - c) / n
+    I = f(a, c, z) + f(a, c, z) + f(a, d, z) + f(b, d, z)
+    
+    for i in range(n) :
+        xi = a+ i*h
+        yj = c +i*k
+        I += 2 * ( f(xi, c, z) + f(xi, d, z) + f(a, yj, z) + f(b, yj, z) )
+    
+    for i in range(n):
+        for j in range(n) :
+            I += 4 * f(a+i*h, c+j*k, z)
+    
+    # Multiply our integral by constants and return it        
+    I *= G * z * h * k * sigma / 4
+    return I
+
+
+# 
+
+y = []
+x = np.arange(.1,10.1,.1)
+
+
+
+for i in range(1,101) :
+    z = i*.1
+    fz = trapezoidal_2D(z)
+    y.append(fz)
+
+
+pl.plot(x,y)
+pl.xlabel("Z, m")
+pl.ylabel("Fz, N")
+pl.title("Plot of Force on a Point Mass")
+pl.show()
--- Exercise 5.21.py
+++ Exercise 5.21.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Exrcise 5.21: Electric Field of a Charge Distribution (Pg. 209)
-
 Created on Sat Oct  7 15:16:50 2017
-
 @author: Maxwell
 """
 
@@ -13,40 +11,71 @@ import pylab as pl
 
 pi = np.pi
 e_0 = 8.85e-12
+q = 1
 
-def electric_potential(q=1):
+def electric_potential():
     return lambda x,y=0: ( q / ( 4*pi*e_0*np.sqrt( (x-5)**2 + y**2 ) ) ) - ( q / ( 4*pi*e_0*np.sqrt( (x+5)**2 + y**2 ) ) )
 
+def e_x():
+    return lambda x,y: -(q/(4*pi*e_0)) * ( (x + 5)/((x+5)**2 + y*y)**(3/2) - (x-5)/((x-5)**2 + y*y)**(3/2)  )
 
+def e_y():
+    return lambda x,y: -(q*y/(4*pi*e_0)) * ( ((x+5)**2 + y*y )**(-3/2) - ((x-5)**2 + y*y)**(-3/2) )
+
+def E_field(x,y):
+    return ( e_x()(x,y), e_y()(x,y) )
 
 # ======= Make a density plot of Phi(x,y) ======= #
 
 
 Phi = electric_potential()
 
-xrange = 15
-yrange = 15
+# Change xrange and yrange to 50 for part a
+xrange = 20
+yrange = 20
 points = 2 * xrange
 xspace = 2*xrange / (points)
 yspace = 2*yrange / (points)
 
-arr = np.empty([points, points], float)
+arr = np.zeros([points, points], float)
 
 for i in range(0, points):
     y = i*yspace - yrange
     for j in range(0, points):
         x = j*xspace - xrange
         if (x-5)**2 + y*y == 0 or (x+5)**2 + y*y == 0:
+            arr[i,j] = 0
             continue
         arr[i,j] = Phi(x,y)
 
 
 
-pl.imshow(arr, origin="lower", extent=[-xrange,xrange, -yrange,yrange])
+pl.imshow(arr, origin="lower", extent=[0,2*xrange, 0,2*yrange])
+pl.title("Electric Potential on the X-Y Plane")
+pl.hsv()
 pl.show()
 
 
 # ======= Make a density plot of E(x,y) (magnitude & direction plots) ====== #
+
+mag = np.zeros([points, points], float)
+
+for i in range(0, points):
+    y = i*yspace - yrange
+    for j in range(0, points):
+        x = j*xspace - xrange
+        if (x-5)**2 + y*y == 0 or (x+5)**2 + y*y == 0:
+            mag[i,j] = 0
+            continue
+        field = E_field(x,y)
+        mx = field[0]
+        my = field[1]
+        mag[i,j] = np.sqrt(mx*mx + my*my)
+
+pl.imshow(mag, origin="lower", extent=[0,2*xrange, 0,2*yrange])
+pl.title("Magnitude of E field on the X-Y Plane")
+pl.bone()
+pl.show()
 
 
 
@@ -54,9 +83,11 @@ pl.show()
 
 
 # ======= Fun: Make a plot of Phi at y=0 ======= #
+"""
 x = np.linspace(-50,50,100)
 pl.plot(x, Phi(x))
-pl.title("Plot of Electrix Potential Vs x at y=0")
+pl.title("Plot of Electric Potential Vs x at y=0")
 pl.xlabel("x, cm")
 pl.ylabel("Phi, V")
 pl.show()
+"""
