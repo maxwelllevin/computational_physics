d94c6e514667c39bff20859bc2bcec0afbbd5083
Maxwell <maxwellevin@users.noreply.github.com>
2017-10-10 22:48:07 -0700

    Add files via upload

--- Exercise 5.21.py
+++ Exercise 5.21.py
@@ -39,22 +39,22 @@ def charge_density(x,y, Lx=5, Ly=5):
 
 Phi = electric_potential()
 
-# Change xrange and yrange to 50 for part a
-xrange = 15
-yrange = 15
+# Change x_range and y_range to 50 for part a
+x_range = 50
+y_range = 50
 
 
 res = 1
-points = 2 * xrange * res
-xspace = 2*xrange / (points)
-yspace = 2*yrange / (points)
+points = 2 * x_range * res
+xspace = 2*x_range / (points)
+yspace = 2*y_range / (points)
 
 arr = np.zeros([points, points], float)
 
 for i in range(0, points):
-    y = i*yspace - yrange
+    y = i*yspace - y_range
     for j in range(0, points):
-        x = j*xspace - xrange
+        x = j*xspace - x_range
         if (x-5)**2 + y*y == 0 or (x+5)**2 + y*y == 0:
             arr[i,j] = 0
             continue
@@ -62,7 +62,7 @@ for i in range(0, points):
 
 
 print("\n\nNegative charge on the left, Positive charge on the right\n")
-pl.imshow(arr, origin="lower", extent=[-xrange,xrange, -yrange,yrange])
+pl.imshow(arr, origin="lower", extent=[-x_range,x_range, -y_range,y_range])
 pl.title("Electric Potential on the X-Y Plane")
 pl.xlabel("x, cm")
 pl.ylabel("y, cm")
@@ -73,18 +73,18 @@ pl.show()
 # ======= Make a density plot of E(x,y) (magnitude & direction plots) ====== #
 
 # Magnitude Plot
-mag = np.zeros([2*xrange, 2*xrange], float)
-for x in range(-xrange, xrange):
-    for y in range(-yrange, yrange):
+mag = np.zeros([2*x_range, 2*x_range], float)
+for x in range(-x_range, x_range):
+    for y in range(-y_range, y_range):
         if (x-5)**2 + y*y == 0 or (x+5)**2 + y*y == 0:
             mag[y,x] = 0
             continue
         field = E_field(x,y)
         mx = field[0]
         my = field[1]
-        mag[y+yrange, x+xrange] = np.sqrt(mx*mx + my*my) # Have to reverse the index?
+        mag[y+y_range, x+x_range] = np.sqrt(mx*mx + my*my) # Have to reverse the index?
 
-pl.imshow(mag, origin="lower", extent=[-xrange,xrange, -yrange,yrange])
+pl.imshow(mag, origin="lower", extent=[-x_range,x_range, -y_range,y_range])
 pl.title("Magnitude of E field on the X-Y Plane")
 pl.xlabel("x, cm")
 pl.ylabel("y, cm")
@@ -93,18 +93,17 @@ pl.show()
 
 
 # Direction Plot - HELP
-direct = np.zeros([2*xrange, 2*xrange], float)
-for x in range(-xrange, xrange):
-    for y in range(-yrange, yrange):
+direct = np.zeros([2*x_range, 2*x_range], float)
+for x in range(-x_range, x_range):
+    for y in range(-y_range, y_range):
         if (x-5)**2 + y*y == 0 or (x+5)**2 + y*y == 0:
-            direct[y,x] = 0
             continue
         field = E_field(x,y)
         dirx = field[0]
         diry = field[1]
-        direct[y+yrange, x+xrange] = dirx + diry  # Have to reverse the index?
+        direct[y+y_range, x+x_range] = np.arctan(diry/dirx)  #dirx + diry  # Have to reverse the index?
 
-pl.imshow(direct, origin="lower", extent=[-xrange,xrange, -yrange,yrange])
+pl.imshow(direct, origin="lower", extent=[-x_range,x_range, -y_range,y_range])
 pl.title("Direction of E field on the X-Y Plane")
 pl.xlabel("x, cm")
 pl.ylabel("y, cm")
@@ -113,6 +112,7 @@ pl.show()
 
 # ====== Make a density plot of E field with new charge distribution ======#
 
+print("\nPart c)\n")
 
 #
 #
@@ -120,16 +120,38 @@ pl.show()
 #
 #
 
-Lx = xrange
-Ly = yrange
+L = x_range
+
+# Function that computes the integral of a given function from set bounds with specific depth (~accuracy)
+def simpson_integral(lower_bound=0, upper_bound=L, function=lambda x: 1, depth=0.1):
+    N = int( (upper_bound - lower_bound) / depth )
+    ans = ( function(lower_bound) + function(upper_bound))
+    
+    for k in range(1, N, 2):
+        ans += 4 * function(lower_bound + k * depth)
+    
+    for k in range(2, N-1, 2):
+        ans += 2 * function(lower_bound + k * depth)
+    
+    ans *= depth / 3
+    return ans
+
+def new_E_inside(x1,y1):
+    return lambda x,y: (q_0*np.sin(2*pi*x/L)*np.sin(2*pi*y/L) ) / ( 4*pi*e_0*np.sqrt( (x-x1)**2 + (y-y1)**2 ) )
+
+def new_E_x():
+    return 0
+
+
+
 
 # Plot of charge distribution
-char = np.zeros([2*Lx, 2*Ly], float)
-for x in range(-Lx, Lx):
-    for y in range(-Ly, Ly):
-        char[y+Ly, x+Lx] = charge_density(x,y, Lx, Ly) # Have to reverse the index?
+char = np.zeros([2*L, 2*L], float)
+for x in range(-L, L):
+    for y in range(-L, L):
+        char[y+L, x+L] = charge_density(x,y, L, L) # Have to reverse the index?
 
-pl.imshow(char, origin="lower", extent=[-xrange,xrange, -yrange,yrange])
+pl.imshow(char, origin="lower", extent=[-x_range,x_range, -y_range,y_range])
 pl.title("Magnitude of E field on the X-Y Plane")
 pl.xlabel("x, cm")
 pl.ylabel("y, cm")
